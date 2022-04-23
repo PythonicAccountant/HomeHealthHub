5795eeab3478421eded095a6d2f75eb99119e020
PythonicAccountant <44148347+PythonicAccountant@users.noreply.github.com>
2022-04-23 12:03:18 -0700

    initial commit

---
+++ home_health_hub/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from home_health_hub.users.models import User
+from home_health_hub.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
