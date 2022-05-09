682ae96542f2d23b81b4196f59dcb88b914ac7e5
Danny Arguello <44148347+PythonicAccountant@users.noreply.github.com>
2022-05-09 14:04:01 -0700

    fix filter view

--- home_health_hub/calorietracker/filters.py
+++ home_health_hub/calorietracker/filters.py
@@ -9,17 +9,19 @@ from .models import FoodLogItem
 
 
 class FoodLogItemFilter(django_filters.FilterSet):
-    user__username = django_filters.ModelChoiceFilter(queryset=User.objects.all())
+    user = django_filters.ModelChoiceFilter(
+        field_name="food_log__user", queryset=User.objects.all()
+    )
     weeks = django_filters.NumberFilter(
-        field_name="date_eaten", method="get_past_n_weeks", label="Past n weeks"
+        field_name="food_log__date", method="get_past_n_weeks", label="Past n weeks"
     )
 
     def get_past_n_weeks(self, queryset, field_name, value):
         time_threshold = timezone.now() - timedelta(weeks=int(value))
-        return queryset.filter(date_eaten__gte=time_threshold)
+        return queryset.filter(food_log__date__gte=time_threshold)
 
     class Meta:
         model = FoodLogItem
         fields = [
-            "user__username",
+            "food_log__user",
         ]
