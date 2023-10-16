607f5b84f88ff03aa2aa0ed951c9b361bfc59102
PythonicAccountant <44148347+PythonicAccountant@users.noreply.github.com>
2023-10-16 13:05:25 -0700

    basic view for all days in month

--- home_health_hub/weighttracker/models.py
+++ home_health_hub/weighttracker/models.py
@@ -19,11 +19,12 @@ class WeightProfile(models.Model):
 class WeightLog(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date = models.DateField()
-    weight = models.DecimalField(max_digits=10, decimal_places=1)
-    trend = models.DecimalField(max_digits=10, decimal_places=1, blank=True)
+    weight = models.DecimalField(max_digits=10, decimal_places=1, blank=True, null=True)
+    trend = models.DecimalField(max_digits=10, decimal_places=1, blank=True, null=True)
 
     class Meta:
         get_latest_by = "date"
+        ordering = ["date"]
         constraints = [
             models.UniqueConstraint(
                 fields=["user", "date"], name="unique WeightLog per user"
@@ -55,7 +56,21 @@ class WeightLog(models.Model):
             return 0
 
     @staticmethod
-    def get_days(month, year):
-        date = datetime.datetime(year=year, month=month, day=1)
-        days = calendar.monthrange(date.year, date.month)[1]
-        return [date.replace(day=day) for day in range(1, days + 1)]
+    def get_or_create_days(user, year, month):
+        no_days_in_month = calendar.monthrange(year, month)[1]
+        logs = WeightLog.objects.filter(user=user, date__year=year, date__month=month)
+        if len(logs) == no_days_in_month:
+            return logs
+        else:
+            date = datetime.date(year=year, month=month, day=1)
+            days_in_month = [
+                date.replace(day=day) for day in range(1, no_days_in_month + 1)
+            ]
+            WeightLog.objects.bulk_create(
+                [WeightLog(user=user, date=day) for day in days_in_month],
+                ignore_conflicts=True,
+            )
+            logs = WeightLog.objects.filter(
+                user=user, date__year=year, date__month=month
+            )
+            return logs
--- home_health_hub/weighttracker/views.py
+++ home_health_hub/weighttracker/views.py
@@ -1,3 +1,21 @@
-# from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.template.response import TemplateResponse
+
+from .models import WeightLog
+
+
+@login_required
+def month_dash(request, year=datetime.today().year, month=datetime.today().month):
+    context = {
+        "days": WeightLog.get_or_create_days(request.user, year, month),
+        "month": month,
+        "year": year,
+    }
+
+    if not request.META.get("HTTP_HX_REQUEST"):
+        return TemplateResponse(request, "weighttracker/monthlydash.html", context)
+    return TemplateResponse(
+        request, "weighttracker/fragments/monthlydash_fragment.html", context
+    )
