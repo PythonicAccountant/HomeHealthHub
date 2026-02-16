81614812ea46e909bac8dde51c50e2cfecfadaee
PythonicAccountant <44148347+PythonicAccountant@users.noreply.github.com>
2026-02-15 23:08:39 -0800

    fix signal import

--- home_health_hub/tracker/apps.py
+++ home_health_hub/tracker/apps.py
@@ -5,5 +5,5 @@ class TrackerConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "home_health_hub.tracker"
 
-    # def ready(self):
-    #     import tracker.signals
+    def ready(self):
+        import tracker.signal  # noqa
